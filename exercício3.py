0e1de772b32be24164e951e17e6c6536da0c087e
Thaynara19 <144859274+Thaynara19@users.noreply.github.com>
2024-02-07 17:57:32 -0300

    Create exercício3.py

---
+++ exercício3.py
@@ -0,0 +1,5 @@
+#Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
+va1 = int(input())
+va2 = int(input())
+PROD = va1 * va2
+print("PROD =",PROD)
