9fc66ce0a391be6b46a19590c25e620de4536a56
Thaynara Alves <144859274+Thaynara19@users.noreply.github.com>
2024-02-14 19:20:02 -0300

    Create 1013.py

---
+++ 1013.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+MAIOR AB=(a+b+abs(a-b))/2
+
+valor = input().split()
+A = int(valor[0])
+B = int(valor[1])
+C = int(valor[2])
+MAIOR = ((A+B)+abs(A-B))/2
+RES = ((MAIOR +C)+abs(MAIOR - C))/2
+print("%d eh maior"%RES)
